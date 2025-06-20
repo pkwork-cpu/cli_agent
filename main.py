918eca0e43e8efd1f905c43f3033c20f38420f4c
pkwork-cpu <kermani.pedram6@gmail.com>
2025-06-19 22:27:41 -0500

    Ch1L5: Messages

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 
 if len(sys.argv) < 2:
         print ("Usage: python3 main.py <prompt>")
@@ -15,7 +17,14 @@ api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+#sys.argv[1]
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=sys.argv[1]
+    model='gemini-2.0-flash-001',
+    contents=messages,
 )
+
 print(f"{response.text}\nPrompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
