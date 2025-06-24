43afe17013de2744d1352606ea48efc06cac41e4
pkwork-cpu <kermani.pedram6@gmail.com>
2025-06-23 22:20:07 -0500

    main.py updated

--- main.py
+++ main.py
@@ -4,30 +4,44 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+def main():
+    load_dotenv()
 
-if len(sys.argv) < 2:
-        print ("Usage: python3 main.py <prompt>")
+    verbose = '--verbose' in sys.argv
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
+    
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-user_prompt = sys.argv[1]
+    user_prompt = " ".join(args)
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages, verbose)
 
 
-client = genai.Client(api_key=api_key)
-
-
-messages = [
-    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
-]
-#sys.argv[1]
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001',
-    contents=messages,
-)
-
-if '--verbose' in sys.argv:
-    print(f"{response.text}\nUser prompt: {user_prompt}\nPrompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
-else:
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
     print(response.text)
+
+
+if __name__ == "__main__":
+    main()
