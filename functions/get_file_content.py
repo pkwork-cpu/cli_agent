b0916d44f061981bde76968ac3f15e8523f22d02
pkwork-cpu <kermani.pedram6@gmail.com>
2025-07-11 19:01:16 -0500

    Ch2L2 updated

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,26 +1,21 @@
 import os
+from config import MAX_CHARS
+
 
 def get_file_content(working_directory, file_path):
     abs_working_dir = os.path.abspath(working_directory)
-    target_file = abs_working_dir
-    if file_path:
-        target_file = os.path.abspath(os.path.join(working_directory, file_path))
-    else:
-        return "Error: No file specified"
-    if not target_file.startswith(abs_working_dir):
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-    if not os.path.isfile(target_file):
+    if not os.path.isfile(abs_file_path):
         return f'Error: File not found or is not a regular file: "{file_path}"'
     try:
-        MAX_CHARS = 10000
-        file_content = []
-        with open(target_file, "r") as f:
-            content = f.read(MAX_CHARS + 1)
-            truncated = len(content) > MAX_CHARS
-            file_content_string = content[:MAX_CHARS]
-            file_content.append(file_content_string)
-            if truncated:
-                file_content.append(f'...File "{file_path}" truncated at 10000 characters')
-        return "\n".join(file_content)
+        with open(abs_file_path, "r") as f:
+            content = f.read(MAX_CHARS)
+            if os.path.getsize(abs_file_path) > MAX_CHARS:
+                content += (
+                    f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                )
+        return content
     except Exception as e:
-        return f"Error reading file: {e}"
+        return f'Error reading file "{file_path}": {e}'
--- tests.py
+++ tests.py
@@ -3,26 +3,14 @@ from functions.get_file_content import get_file_content
 
 def test():
     result = get_file_content("calculator", "main.py")
-    print("Result for 'main.py':")
     print(result)
-    print("====================================")
 
     result = get_file_content("calculator", "pkg/calculator.py")
-    print("Result for 'pkg/calculator.py':")
     print(result)
-    print("====================================")
 
     result = get_file_content("calculator", "/bin/cat")
-    print("Result for '/bin/cat':")
     print(result)
-    print("====================================")
 
 
-    '''
-    result = get_file_content("calculator", "lorem.txt")
-    print("Result for 'lorem.txt':")
-    print(result)
-    print("")
-    '''
 if __name__ == "__main__":
     test()
