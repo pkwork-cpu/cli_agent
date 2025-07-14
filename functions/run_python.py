036c39821e6fe7accbe4400c038e80c741f3d8af
pkwork-cpu <kermani.pedram6@gmail.com>
2025-07-14 00:51:32 -0500

    Ch2L5

---
+++ functions/run_python.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+
+def run_python_file(working_directory, file_path):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(abs_file_path):
+        return f'Error: File "{file_path}" not found'
+    if not abs_file_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+    try:
+        result = subprocess.run('uv run' + file_path, timeout=30, capture_output=True, shell=True)
+        final_result = []
+        final_result.append(f"STDOUT:\n{result.stdout}")
+        final_result.append(f"STDERR:\n{result.stderr}")
+        if result.returncode != 0:
+            final_result.append(f"process exited with code {result.returncode}")
+        if not result.stdout:
+            final_result.append("No output produced.")
+        return "\n".join(final_result)
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
