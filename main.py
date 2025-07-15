36076187c2191f3e89fe8b27a882a6ba8554767c
pkwork-cpu <kermani.pedram6@gmail.com>
2025-07-15 15:03:35 -0500

    Ch3L1 updated

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from prompts import system_prompt
+
 def main():
     load_dotenv()
 
@@ -13,7 +15,7 @@ def main():
     if not args:
         print("AI Code Assistant")
         print('\nUsage: python main.py "your prompt here" [--verbose]')
-        print('Example: python main.py "How do I build a calculator app?"')
+        print('Example: python main.py "How do I fix the calculator?"')
         sys.exit(1)
     
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -32,7 +34,6 @@ def main():
 
 
 def generate_content(client, messages, verbose):
-    system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
